da5164fe574f8e75034261680b1b3f1802cfb655
Onassis Sowah Anyetei <onassis.sowah@gmail.com>
2022-06-30 20:02:29 +0200

    ui changes

--- src/components/Note/Note.jsx
+++ src/components/Note/Note.jsx
@@ -1,17 +1,18 @@
-import React from 'react';
-import './note.css';
-import { MdDeleteForever } from 'react-icons/md';
+import React from 'react'
+import './note.css'
+import { MdDeleteForever } from 'react-icons/md'
 
 function Note({ Key, text, date }) {
-	return (
-		<div className='note_container'>
-			<span>{text}</span>
-			<div className='note_footer'>
-				<small>{date}</small>
-				<MdDeleteForever className='delete_icon' />
-			</div>
-		</div>
-	);
+  return (
+    <div className="note_container">
+      <span>{text}</span>
+
+      <div className="note_footer">
+        <small>{date}</small>
+        <MdDeleteForever className="delete_icon" />
+      </div>
+    </div>
+  )
 }
 
-export default Note;
+export default Note
